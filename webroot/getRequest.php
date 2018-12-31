94d048c24f05bbde751923b48f559414aa8baa33
ImMortex <44499173+ImMortex@users.noreply.github.com>
2018-12-31 23:53:03 +0100

    Add files via upload

---
+++ webroot/getRequest.php
@@ -0,0 +1,13 @@
+<?php
+
+//var_dump($_GET);
+
+foreach ($_GET as $k => $v) {
+    echo $k . " = " . $v . "<br>\n";
+
+    //Kommentar: Punkte konkatenieren Strings in PHP
+}
+
+echo $_GET['name'];
+
+?>
---
+++ webroot/test.php
@@ -0,0 +1,13 @@
+<!Doctype html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+  
+</head>
+<body>
+    <?php
+
+    echo "<h1>Testausgabe</h1>";
+    ?>
+</body>
+</html>
